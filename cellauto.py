9b15baa791a6308e338520978938c70e82d3f37d
RAymond <raymondz64@nycstudents.net>
2026-05-13 10:10:05 -0400

    77

--- cellauto.py
+++ cellauto.py
@@ -1,31 +1,43 @@
 import numpy as np
-
+# colors: green and red
+clrs = [[255,0,0], [0,255,0]]
 def setup_pixels():
-    state = [[82, 107, 49],[88, 76, 58],[16, 12, 8],[215, 207, 206]] #list of the colors 
-    for y in range(height): #loop every y value 
-        for x in range(width): #loop every x value
-            np_pixels[y,x,1:] = state[int(random(4))] #set the color
-    update_np_pixels() #load friggin pixels yo
-    
-def setup():
-    global p
-    size(200,200)
-    p = [[]]
-    background(0)
-    load_np_pixels()
-    setup_pixels()
-    
-def evaluate_neighbors():
-    global p
-    for y in range(1, height-1):
-        for x in range(1, width-1):
-            np_pixels[y-1:y+1, x-1:x+1, 1:]
-            
-            
-    
-def trans():
+    for x in range(width):
+        for y in range(height):
+            np_pixels[y,x,1:] = clrs[ int(random(2)) ]
     update_np_pixels()
     
-def draw():
-    evaluate_neighbors()
-    trans()
+def setup():
+  global s
+  size(250,250)
+  s=[[ [[0,0,0] for i in range(8)] for x in range(width)] for y in    range(height)] # empty state list
+  background(0) # black
+  load_np_pixels()
+  setup_pixels()
+  
+def evaluate_neighbors():
+  global s
+  for x in range(1,width-1):   # ignore edges
+    for y in range(1,height-1):
+
+      # grab the colors of all 8 neighbors (and itself)
+      s[x][y]=[ list(c) for pxls in np_pixels[y-1:y+2, x-1:x+2, 1:] for c in pxls ]
+      
+      # take out the pixel itself
+      s[x][y].pop(4)
+      
+def transition():
+    for x in range(1,width-1):
+        for y in range(1,height-1):
+          # check for each color
+          inx = [0]*len(clrs) # use the index as a color count
+          for clr in s[x][y]:
+              for i in range(2):
+                  if clr == clrs[i]:
+                    inx[i] +=1 # index of the matching color
+     # grab the index of the mode	      
+          clr_mode = inx.index( max(inx) )
+          # update that pixel’s color		
+          np_pixels[y,x,1:] = clrs[ clr_mode ] 
+    update_np_pixels()
+
